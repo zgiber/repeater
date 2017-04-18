3befbb48c3adbea389e8562d2e1d6b9ec0313b9d
Zoltan Giber <zoltan@typeform.com>
2017-04-18 10:04:15 +0200

    added a simple view

--- main.go
+++ main.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
+	"html/template"
 	"io"
+	"log"
 	"net/http"
 	"sync"
 )
+
+type payload struct {
+	NumParallelConsumers int       `json:"number_of_parallel_consumers"`
+	ConsumerBufferSize   int       `json:"parallel_buffer_size"`
+	ConsumerBufferUsage  []float64 `json:"parallel_buffers_usage_perc"`
+}
 
 type store struct {
 	sync.RWMutex
@@ -30,10 +39,36 @@ func (srv *service) handleRequests(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func (srv *service) handleGetGUI(w http.ResponseWriter, r *http.Request) {
+
+	data := &payload{}
+	err := json.Unmarshal(srv.store.data.Bytes(), data) // if unmarshal fails we just display empty page
+	if err != nil {
+		log.Print(err)
+	}
+
+	viewTemplate := `<!DOCTYPE html><html>
+	<head><meta http-equiv="refresh" content="2"></head>
+	<body>
+	Load on workers <br>
+	{{range .ConsumerBufferUsage}}<div>{{ . }}%</div>{{end}}</body></html>`
+
+	t, err := template.New("view").Parse(viewTemplate)
+	if err != nil {
+		log.Print("1", err)
+		return
+	}
+	err = t.Execute(w, data)
+	if err != nil {
+		log.Print("2", err)
+	}
+}
+
 func main() {
 	srv := newService()
 	http.HandleFunc("/", srv.handleRequests)
-	http.ListenAndServe(":8000", nil)
+	http.HandleFunc("/gui", srv.handleGetGUI)
+	http.ListenAndServe(":8008", nil)
 }
 
 func newService() *service {
